cfd1cba4bb95f7b6936cac0e646bea49db03a31e
MihirJayswal812007 <mihirjayswal812007@gmail.com>
2026-01-04 04:30:26 +0530

    3

--- ai_hallucination/database.py
+++ ai_hallucination/database.py
@@ -13,7 +13,7 @@ if not DATABASE_URL:
     raise ValueError("DATABASE_URL is not set. Check your Render Environment tab!")
 
 # Future=True is now default in 2.0, but echo=True helps you see SQL logs in Render
-engine = create_async_engine(DATABASE_URL, echo=True)
+engine = create_async_engine(DATABASE_URL, echo=True, connect_args={"ssl": True})
 
 # Use async_sessionmaker for cleaner session creation
 async_session_factory = async_sessionmaker(
