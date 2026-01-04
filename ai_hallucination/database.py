7173edd12cbc9fb4c15c648bb5dcf064df92eaff
MihirJayswal812007 <mihirjayswal812007@gmail.com>
2026-01-04 05:40:16 +0530

    0

--- ai_hallucination/database.py
+++ ai_hallucination/database.py
@@ -8,11 +8,14 @@ load_dotenv()
 DATABASE_URL = os.getenv("DATABASE_URL")
 
 if not DATABASE_URL:
-    raise ValueError("DATABASE_URL is not set!")
+    raise ValueError("DATABASE_URL is not set")
 
 engine = create_async_engine(
     DATABASE_URL,
-    echo=True
+    echo=True,
+    connect_args={
+        "ssl": True
+    }
 )
 
 async_session_factory = async_sessionmaker(
